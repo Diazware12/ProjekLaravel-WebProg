7bfec67d1c8e6a65d6616b8f58f9f731c70d3b88
diazware12 <diazwalker98@gmail.com>
2019-12-18 19:38:39 +0700

    revisi

--- routes/web.php
+++ routes/web.php
@@ -11,56 +11,7 @@
 |
 */
 
-/*
-//guest
-    Route::get('/', function () {
-        return view('homePage');
-    });
 
-    Route::get('/login', function () {
-        return view('login');
-    });
-
-    Route::get('/register', function () {
-        return view('register');
-    });
-
-    
-    Route::get('/c', function () {
-        return view('movieDetail');
-    });
-
-    Route::get('/d', function () {
-        return view('profile');
-    });
-
-// member
-    
-    Route::get('/e', function () {
-        return view('member.memberHome');
-    });
-    Route::get('/f', function () {
-        return view('member.memberMovieDetail');
-    });
-
-    Route::get('/g', function () {
-        return view('member.memberProfile');
-    });
-
-    Route::get('/h', function () {
-        return view('member.otherProfile');
-    });
-
-    Route::get('/i', function () {
-        return view('member.inbox');
-    });
-
-    Route::get('/j', function () {
-        return view('member.savedMovie');
-    });
-
-
-*/
 
 
 Auth::routes();
